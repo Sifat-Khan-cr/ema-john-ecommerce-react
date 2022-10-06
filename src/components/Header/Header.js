db93db45e59afb25692bcced5c7840d372f666b0
Sifat-Khan-cr <pskcrjr@gmail.com>
2022-10-07 01:56:08 +0600

    navbar routed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
         <nav className='header'>
             <img src={logo} alt="" />
             <div>
-                <a href="/shop">Shop</a>
+                <a href="/">Shop</a>
                 <a href="/orders">Orders</a>
                 <a href="/inventory">Inventory</a>
                 <a href="/about">About</a>
